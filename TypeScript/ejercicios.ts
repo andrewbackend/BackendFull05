3c1c57092b382befc9cf0e09fbaabd7d959dadfe
el mercadito <andrerami0330@gmail.com>
2024-11-20 22:03:29 -0500

    se agregó un ejercició más de TS

--- TypeScript/ejercicios.ts
+++ TypeScript/ejercicios.ts
@@ -1,4 +1,5 @@
 /*
+01.-
 Implementa un tipo genérico llamado SoloCadena<T> que transforme las propiedades de 
 un tipo dado T:
 
@@ -17,4 +18,19 @@ Crear un objeto de tipo UsuarioSoloCadena donde:
 - Las propiedades que son cadenas en la interfaz original (nombre) sigan siendo cadenas.
 - Las propiedades que no con cadenas (edad, activo) conserven sus tipos originales.
 
+*/
+
+
+/*
+02.-
+Crea una función que reciba un DNI(8 números) y calculque la ltra del DNI
+
+Es un proceso matemático fácil que se basa en obtener el resto de la división
+entera del número de DNI y el número 23.
+Y con el resto se obtiene la letra, susandolo como posición o índice dentro
+de un array de letras.
+
+Este sería el array de letras:
+const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 
+'B', 'N', 'J','Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 */
